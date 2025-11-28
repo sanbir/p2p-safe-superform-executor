ed9a3bb7dda76a375b004021a1eeb6bada106156
alexander.biryukov <alexander.biryukov@p2p.org>
2025-11-28 14:37:00 +0400

    @p2p-org/safe-superform-executor@0.1.0

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {},
+    },
+  },
+  rules: {
+    'import/order': 'off',
+    'import/no-duplicates': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+}
